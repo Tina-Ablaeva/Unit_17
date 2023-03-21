bd5e3a38fcba7437deb8056c77058148638debb8
Valentina Ablaeva <125043384+Tina-Ablaeva@users.noreply.github.com>
2023-03-21 21:03:48 +0700

    Delete Task_17_7_3.py

--- Task_17_7_3.py
+++
@@ -1,8 +0,0 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = int(input("Введите сумму, которую планируете внести в банк: "))
-m_new = money/100
-deposit = [int(per_cent.get("ТКБ")*m_new), int(per_cent.get("СКБ")*m_new),
-           int(per_cent.get("ВТБ")*m_new), int(per_cent.get("СБЕР")*m_new)]
-print("Ставки, которые предлагают банки на сегодняшний день: ", list(per_cent.values()))
-print("Доход, который будет получен от размещения средств:   ", deposit)
-print("При внесении", money, "максимальная выгода от размещения составит: ", max(deposit))
